5981568e2ad5f24e17700d46353ac6d976f0a14e
Stefany Biskup <33550545+teffybiskup@users.noreply.github.com>
2025-06-08 12:21:36 -0700

    Update Counter.spec.tsx

--- src/tests/Counter.spec.tsx
+++ src/tests/Counter.spec.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import '@testing-library/jest-dom';
 import { render, fireEvent, screen } from '@testing-library/react';
 import Counter from '../components/Counter/Counter';
 
